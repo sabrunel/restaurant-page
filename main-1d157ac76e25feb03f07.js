24be716593a7794005135685964b3a660b868206
Samantha Brunel <59365424+sabrunel@users.noreply.github.com>
2023-03-21 20:20:17 +0100

    Setup project

---
+++ main-1d157ac76e25feb03f07.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Hello world!")
+/******/ })()
+;
+//# sourceMappingURL=main-1d157ac76e25feb03f07.js.map
